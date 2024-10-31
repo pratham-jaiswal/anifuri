ce085b3f5b1c213feea6f1a2d2fafc61efee5043
pratham-jaiswal <prathamj0502@gmail.com>
2024-10-31 18:31:31 +0530

    Add bottom tabbar

---
+++ client/app/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(screens)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}

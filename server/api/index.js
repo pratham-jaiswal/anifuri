72c4f0cd55b9c1415e9337524a13d0a2c1d19c82
pratham-jaiswal <prathamj0502@gmail.com>
2024-11-01 20:25:54 +0530

    Add redis caching

--- server/api/index.js
+++ server/api/index.js
@@ -1,16 +1,38 @@
 import { HiAnime } from "aniwatch";
 import express from "express";
 import cors from "cors";
+import compression from "compression";
+import redis from "redis";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(compression());
+
+const redisClient = redis.createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_URL,
+        port: process.env.REDIS_PORT,
+    }
+});
+
+redisClient.connect().catch(console.error);
 
 const hianime = new HiAnime.Scraper();
 
-app.get("/explore", (req, res) => {
+app.get("/explore", async(req, res) => {
+  const cacheKey = "explore";
+  const cachedData = await redisClient.get(cacheKey);
+  if (cachedData) {
+    return res.send(JSON.parse(cachedData));
+  }
+
   hianime
     .getHomePage()
     .then((data) => {
@@ -37,16 +59,25 @@ app.get("/explore", (req, res) => {
         }
       }
 
+      redisClient.setEx(cacheKey, 60 * 60 * 3, JSON.stringify(formattedData));
       res.send(formattedData);
     })
     .catch((err) => console.error(err));
 });
 
-app.get("/search", (req, res) => {
+app.get("/search", async(req, res) => {
   const query = req.query.query;
+  const cacheKey = `search:${query}`;
+
+  const cachedData = await redisClient.get(cacheKey);
+  if (cachedData) {
+    return res.send(JSON.parse(cachedData));
+  }
+
   hianime
     .search(query)
     .then((data) => {
+      redisClient.setEx(cacheKey, 60 * 60 * 12, JSON.stringify(data.animes));
       res.send(data.animes);
     })
     .catch((err) => {
@@ -54,27 +85,47 @@ app.get("/search", (req, res) => {
     });
 });
 
-app.get("/anime-info", (req, res) => {
+app.get("/anime-info", async(req, res) => {
   const animeId = req.query.animeId;
+  const cacheKey = `anime-info:${animeId}`;
+
+  const cachedData = await redisClient.get(cacheKey);
+  if (cachedData) {
+    return res.send(JSON.parse(cachedData));
+  }
+
   hianime
     .getInfo(animeId)
     .then((data) => {
       delete data.anime.info.anilistId;
       delete data.anime.info.malId;
-      res.send({ anime: data.anime, seasons: data.seasons });
+
+      const response = { anime: data.anime, seasons: data.seasons };
+      redisClient.setEx(cacheKey, 60 * 60 * 24 * 3, JSON.stringify(response));
+      
+      res.send(response);
     })
     .catch((err) => {
       console.error(err);
     });
 });
 
-app.get("/episodes-list", (req, res) => {
+app.get("/episodes-list", async(req, res) => {
   const animeId = req.query.animeId;
+  const cacheKey = `episodes-list:${animeId}`;
+
+  const cachedData = await redisClient.get(cacheKey);
+  if (cachedData) {
+    return res.send(JSON.parse(cachedData));
+  }
+
   hianime
     .getEpisodes(animeId)
     .then((data) => {
       data.episodes = data.episodes.map((episode) => {
         const epValue = episode.episodeId.split("=")[1];
+        redisClient.setEx(cacheKey, 60 * 60 * 24 * 7, JSON.stringify(data));
+
         return { ...episode, episodeId: epValue };
       });
 
@@ -87,7 +138,15 @@ app.get("/episodes-list", (req, res) => {
 
 app.get("/episode-server-sources", async (req, res) => {
   const animeId = req.query.animeId;
-  const episodeId = req.query.episodeId;
+  const givenEpisodeId = req.query.episodeId;
+  const episodeId = (givenEpisodeId.match(/ep=(\d+)/) || [])[1] || givenEpisodeId.replace(/\D/g, '');
+
+  const cacheKey = `episode-server-sources:${animeId}:${episodeId}`;
+
+  const cachedData = await redisClient.get(cacheKey);
+  if (cachedData) {
+    return res.send(JSON.parse(cachedData));
+  }
 
   try {
     const serverData = await hianime.getEpisodeServers(`${animeId}?ep=${episodeId}`);
@@ -136,13 +195,13 @@ app.get("/episode-server-sources", async (req, res) => {
       await fetchServerSources(server, "dub", subCaptions);
     }
 
+    redisClient.setEx(cacheKey, 60 * 60 * 24 * 30, JSON.stringify(response));
     res.send(response);
   } catch (err) {
     console.error(err);
     res.status(500).send("Error fetching episode details");
   }
 });
-
 
 app.get("/by-genres", (req, res) => {
   const genre = req.query.genre;

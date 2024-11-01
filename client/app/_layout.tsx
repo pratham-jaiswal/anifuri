a5a97ed023904b67ad89ffe4dc291d7ea6b2f1f8
pratham-jaiswal <prathamj0502@gmail.com>
2024-11-01 19:02:52 +0530

    move useeffect to inside component

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -2,22 +2,22 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-useEffect(() => {
-  const hideSplashScreen = async () => {
-    await SplashScreen.preventAutoHideAsync();
-    setTimeout(async () => {
-      await SplashScreen.hideAsync();
-    }, 3000);
-  };
-
-  hideSplashScreen();
-}, []);
-
 export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
+  useEffect(() => {
+    const hideSplashScreen = async () => {
+      await SplashScreen.preventAutoHideAsync();
+      setTimeout(async () => {
+        await SplashScreen.hideAsync();
+      }, 3000);
+    };
+  
+    hideSplashScreen();
+  }, []);
+  
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
